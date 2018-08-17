f8b04b06c34284fd7d4769c96684672025b0e1db
xk1020 <819852084@qq.com>
2018-08-17 16:11:28 +0800

    新增一个算法

---
+++ src/suanFaDemo/demo15.java
@@ -0,0 +1,25 @@
+package suanFaDemo;
+
+/**
+ * @Author: xk
+ * @Date: 2018/8/17
+ * @Version 1.0
+给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+示例:
+输入: [-2,1,-3,4,-1,2,1,-5,4],
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大为6。
+ */
+public class demo15 {
+    public static void main(String[] args){
+
+    }
+}
+class Solution15 {
+    public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        return 0;
+    }
+}
